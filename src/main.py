cb70b0efc66dd2129cf6dba104762627a7a8bd3a
robert-otec <robertotec@outlook.cz>
2025-04-02 18:09:00 +0200

    almost finished generator

--- src/main.py
+++ src/main.py
@@ -1,10 +1,29 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+from copystatic import copy_files_recursive
+from gencontent import generate_page
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
+
 
 def main():
-    # Create a TextNode with some dummy values
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    # Print the object to see its string representation
-    print(node)
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
-if __name__ == "__main__":
-    main()
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating page...")
+    generate_page(
+        os.path.join(dir_path_content, "index.md"),
+        template_path,
+        os.path.join(dir_path_public, "index.html"),
+    )
+
+
+main()
